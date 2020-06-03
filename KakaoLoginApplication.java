68d483b77434df59775d26618a4ff47c27a3f2cf
christianseong <39890852+christianseong@users.noreply.github.com>
2020-06-03 10:46:27 +0900

    Add files via upload

---
+++ KakaoLoginApplication.java
@@ -0,0 +1,14 @@
+package com.example.kakaoLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class KakaoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginApplication.class, args);
+	}
+
+}
